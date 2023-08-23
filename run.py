5320165a5338fd88db5c1c2824ff59a8ee1a0ce2
Lee Curtis <leec313@gmail.com>
2023-08-23 19:39:19 +0000

    Added templates: index.html and about.html

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import os
-from flask import Flask
-
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -8,11 +7,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        ## never have debug=True in a production environment 
         debug=True)
